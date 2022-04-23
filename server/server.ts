189fd42bcc1109ee8e2fd0550a7978d5f8248122
Jhon-Santos68 <61449201+Jhon-Santos68@users.noreply.github.com>
2022-04-23 19:56:17 +0300

    ozgardi

--- server/server.ts
+++ server/server.ts
@@ -1,24 +1,24 @@
-import express from "express";
-import dotenv from 'dotenv';
-import cors from 'cors';
-const connectDB = require('./config/db.ts');
-const app = express();
-dotenv.config();
-connectDB();
+// import express from "express";
+// import dotenv from 'dotenv';
+// import cors from 'cors';
+// const connectDB = require('./config/db.ts');
+// const app = express();
+// dotenv.config();
+// connectDB();
 
 
-const PORT = process.env.PORT;
+// const PORT = process.env.PORT;
 
-app.use(cors())
+// app.use(cors())
 
-app.get('/', (req, res) => {
-  res.send('Salomlar hello!');
-})
+// app.get('/', (req, res) => {
+//   res.send('Salomlar hello!');
+// })
 
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on PORT ${PORT}`)
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is listening on PORT ${PORT}`)
+// });
 
 
 console.log(process.env.MONGO_URL, 'ozgardi 2')
